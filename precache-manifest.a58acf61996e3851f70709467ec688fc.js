772c77cc4cbfc14371ca8eed4948dcd00c9d1199
Raisa <41106327+hipsty3@users.noreply.github.com>
2025-08-04 17:03:00 +0700

    add finalised website

---
+++ precache-manifest.a58acf61996e3851f70709467ec688fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3413170f286350ba12698d81bdd7fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d69d6d3cccb574e41b9a",
+    "url": "/static/css/main.052be0cb.chunk.css"
+  },
+  {
+    "revision": "acea8e61bf651659dec1",
+    "url": "/static/js/2.2cbddb83.chunk.js"
+  },
+  {
+    "revision": "d69d6d3cccb574e41b9a",
+    "url": "/static/js/main.0adacd1b.chunk.js"
+  },
+  {
+    "revision": "509a41322b2586c8d7bd",
+    "url": "/static/js/runtime-main.230ce140.js"
+  },
+  {
+    "revision": "824c18ccebd598609277e2998217111c",
+    "url": "/static/media/azure.824c18cc.png"
+  },
+  {
+    "revision": "a5e5741227e5f5bfb0f3808c9ce4703e",
+    "url": "/static/media/color.a5e57412.jpg"
+  },
+  {
+    "revision": "b7a00da20a203809829743c50fd200a7",
+    "url": "/static/media/foodiy_light.b7a00da2.png"
+  },
+  {
+    "revision": "6b4a1432511ff573ba122abaacd1b50d",
+    "url": "/static/media/gps2.6b4a1432.png"
+  },
+  {
+    "revision": "fee5116d1e4ac166e6fab7f4629c564c",
+    "url": "/static/media/me3.fee5116d.JPG"
+  },
+  {
+    "revision": "60a5eef955a59f7ab10f90f60a32d702",
+    "url": "/static/media/mementos2.60a5eef9.png"
+  },
+  {
+    "revision": "b9aff9f6d60912cc0aadcf20f8d5049d",
+    "url": "/static/media/resume.b9aff9f6.pdf"
+  }
+]);
